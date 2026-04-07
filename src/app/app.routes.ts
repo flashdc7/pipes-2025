2d5f7524f08d5699336dfd7f57e5449f967ab5f4
FlashDC7 <jrda8303@gmail.com>
2026-04-06 19:27:53 -0600

    creación de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'basics',
+    title: 'Pipes comúnes',
+    loadComponent: ()=> import('./pages/basic-page/basic-page'),
+  },
+  {
+    path: 'numbers',
+    title: 'Pipes numericos',
+    loadComponent: ()=> import('./pages/number-page/number-page'),
+  },
+  {
+    path: 'uncommon',
+    title: 'Pipes poco comúnes',
+    loadComponent: ()=> import('./pages/uncommon-page/uncommon-page'),
+  },
+  {
+    path: 'custom',
+    title: 'Pipes personalizados',
+    loadComponent: ()=> import('./pages/custom-page/custom-page'),
+  },
+  {
+    path: '**',
+    redirectTo: 'basics'
+  }
+];
