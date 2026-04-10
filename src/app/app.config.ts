9ca28d75d2097d2385df31b890dc08ed02d92382
FlashDC7 <jrda8303@gmail.com>
2026-04-10 13:00:36 -0600

    agregar idiomas en app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,22 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import localeEs from '@angular/common/locales/es';
+import localeFr from '@angular/common/locales/fr';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEs, 'es');
+registerLocaleData(localeFr, 'fr');
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    {
+      provide: LOCALE_ID,
+      useValue: 'fr'
+    }
   ]
 };
